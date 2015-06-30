91104b7667dbefca24ef77c416a1077048319186
aksndr <aksndr@gmail.com>
2015-06-30 17:12:51 +0300

    Added all files

---
+++ WindowsService/Logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NLog;
+using NLog.Targets;
+using NLog.Config;
+
+namespace WindowsService
+{
+    public class Logger
+    {
+        private NLog.Logger log;
+        public Logger()
+        {           
+            configureNLogger();
+            log = LogManager.GetCurrentClassLogger();
+        }
+
+        private void configureNLogger()
+        {
+            var config = new LoggingConfiguration();                       
+            var fileTarget = new FileTarget();
+            config.AddTarget("file", fileTarget);
+
+            fileTarget.FileName = "${basedir}/log/${shortdate}.log";
+            fileTarget.Layout = "${longdate} ${level} ${message}";
+            fileTarget.KeepFileOpen = false;
+            fileTarget.Encoding = UTF8Encoding.UTF8;
+            fileTarget.CreateDirs = true;
+            
+            fileTarget.ArchiveFileName="${basedir}/archive/{##}.log";
+            fileTarget.ArchiveEvery=FileArchivePeriod.Month;
+            fileTarget.ArchiveNumbering=ArchiveNumberingMode.Rolling;
+            fileTarget.MaxArchiveFiles=12;
+            fileTarget.ConcurrentWrites=true;
+
+            var rule = new LoggingRule("*", LogLevel.Info, fileTarget);
+            config.LoggingRules.Add(rule);
+                        
+            LogManager.Configuration = config;
+        }        
+
+        public void logInfo(string text)
+        {
+            log.Info(text);
+        }
+        public void logWarn(string text)
+        {
+
+            log.Warn(text);                   
+        }
+        public void logError(string text)
+        {
+            log.Error(text);
+        }
+    }
+}
---
+++ WindowsService/Service.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace WindowsService
+{
+    public partial class Service : ServiceBase
+    {
+        private Timer t1 = null;
+        private int step = 0;
+        private Logger logger;
+        public Service()
+        {
+            InitializeComponent();
+            logger = new Logger();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.t1 = new Timer();
+            this.t1.Interval = 30000;
+            this.t1.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            this.t1.Start();
+            logger.logInfo("Test service started");
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            step++;
+            logger.logWarn("Test service step: " + step);
+        }
+
+        protected override void OnStop()
+        {
+            t1.Enabled = false;
+            logger.logWarn("Test service stopped");
+            logger.logError("Test error logging");
+        }
+    }
+}
